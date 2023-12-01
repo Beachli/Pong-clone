eb7b8dfb3b6579d6aa6c67ecd8bdb0da0ab6e3c3
Liam Beach <beachliam48@gmail.com>
2023-11-30 21:11:47 -0600

    Ball is now an object and can bounce off screen edge

--- main.cpp
+++ main.cpp
@@ -5,11 +5,52 @@ int main() {
   InitWindow(800, 600, "Pong"); //Sets up a window
   SetWindowState(FLAG_VSYNC_HINT); //Sets up VSYNC to remove ghosting
 
+  class Ball {
+    public:
+      float x, y;
+      float velocityX, velocityY;
+      float radius;
+
+      void Draw() {
+        DrawCircle((int) x, (int) y, (int) radius, WHITE);
+      }
+
+      Ball(float xval, float yval, float xveloc, float yveloc, float ballrad) {
+        x = xval;
+        y = yval;
+        velocityX = xveloc;
+        velocityY = yveloc;
+        radius = ballrad;
+      }
+  };
+
+  //Ball object construction
+  Ball *ball = new Ball(
+    GetScreenWidth() / 2.0f,
+    GetScreenHeight() / 2.0f,
+    100.0f,
+    300.0f,
+    5.0f
+  );
+
+
   while (!WindowShouldClose()) { //While the window is open
+    ball->x += ball->velocityX * GetFrameTime(); //Move the ball based on velocity
+    ball->y += ball->velocityY * GetFrameTime();
+
+    if (ball->y < 0) {
+      ball->y = 0;
+      ball->velocityY *= -1;
+    }
+    if (ball->y > GetScreenHeight()) {
+      ball->y = GetScreenHeight(); //Stops ball from getting stuck
+      ball->velocityY *= -1; //Invert ball velocity on y axis
+    }
+
     BeginDrawing();
       ClearBackground(BLACK); //Set background to black
       
-      DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 5, WHITE); //Draw ball
+      ball->Draw();//Draw ball
       DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, WHITE); //Draw left paddle
       DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, WHITE); //Draw right paddle
 
@@ -18,5 +59,7 @@ int main() {
   }
   
   CloseWindow();
+  delete ball;
+
   return 0;
 }
