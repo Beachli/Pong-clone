04b38ce94e3f98c57a1dd7356a354af108259061
Liam Beach <beachliam48@gmail.com>
2023-12-01 08:24:35 -0600

    paddles are now objects and can be moved up and down

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <raylib.h>
-#include <string>
 
-int main() {
-  InitWindow(800, 600, "Pong"); //Sets up a window
-  SetWindowState(FLAG_VSYNC_HINT); //Sets up VSYNC to remove ghosting
-
-  class Ball {
+class Ball {
     public:
       float x, y;
       float velocityX, velocityY;
@@ -24,12 +19,51 @@ int main() {
       }
   };
 
-  Ball *ball = new Ball(
-    GetScreenWidth() / 2.0f,
-    GetScreenHeight() / 2.0f,
-    100.0f,
-    300.0f,
-    5.0f
+class Paddle {
+  public:
+    float x, y;
+    float velocity;
+    float width, height;
+
+    void Draw() {
+      DrawRectangle(x - width / 2, y - height / 2, width, height, WHITE);
+    }
+
+    Paddle(float xval, float yval, float veloc, float w, float h) {
+      x = xval;
+      y = yval;
+      velocity = veloc;
+      width = w;
+      height = h;
+    }
+};
+
+int main() {
+  InitWindow(800, 600, "Pong"); //Sets up a window
+  SetWindowState(FLAG_VSYNC_HINT); //Sets up VSYNC to remove ghosting
+
+  Ball *ball = new Ball( //Create ball object
+    GetScreenWidth() / 2.0f, //X pos
+    GetScreenHeight() / 2.0f, //Y pos
+    100.0f, //X velocity
+    400.0f, //Y velocity
+    5.0f //Radius
+  );
+
+  Paddle *leftPaddle = new Paddle( //Create leftPaddle object
+    50, //X pos
+    GetScreenHeight() / 2, //Y pos
+    500, //Velocity
+    10, //Width
+    100
+  );
+
+  Paddle *rightPaddle = new Paddle( //Create rightPaddle object
+    GetScreenWidth() - 50, //X pos
+    GetScreenHeight() / 2, //Y pos
+    500, //Velocity
+    10, //Width
+    100
   );
 
 
@@ -37,6 +71,7 @@ int main() {
     ball->x += ball->velocityX * GetFrameTime(); //Move the ball based on velocity
     ball->y += ball->velocityY * GetFrameTime();
 
+    //Collisions
     if (ball->y < 0) {
       ball->y = 0;
       ball->velocityY *= -1;
@@ -46,12 +81,27 @@ int main() {
       ball->velocityY *= -1; //Invert ball velocity on y axis
     }
 
+    //User input
+    if (IsKeyDown(KEY_W)) {
+      leftPaddle->y -= leftPaddle->velocity * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_S)) {
+      leftPaddle->y += leftPaddle->velocity * GetFrameTime();  
+    }
+    if (IsKeyDown(KEY_UP)) {
+      rightPaddle->y -= rightPaddle->velocity * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_DOWN)) {
+      rightPaddle->y += rightPaddle->velocity * GetFrameTime();
+    }
+
+    //Rendering
     BeginDrawing();
       ClearBackground(BLACK); //Set background to black
       
       ball->Draw();//Draw ball
-      DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, WHITE); //Draw left paddle
-      DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, WHITE); //Draw right paddle
+      leftPaddle->Draw();//Draw left paddle
+      rightPaddle->Draw();//Draw right paddle
 
       DrawFPS(10, 10); //Show FPS
     EndDrawing();
@@ -59,6 +109,8 @@ int main() {
   
   CloseWindow();
   delete ball;
+  delete leftPaddle;
+  delete rightPaddle;
 
   return 0;
 }
