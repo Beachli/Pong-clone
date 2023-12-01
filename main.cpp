42d016c3ad0bfe3080e542f901a9264d7b7d3fad
Liam Beach <beachliam48@gmail.com>
2023-11-30 20:19:00 -0600

    Draws ball and paddles

--- main.cpp
+++ main.cpp
@@ -2,19 +2,18 @@
 #include <string>
 
 int main() {
-  InitWindow(800, 600, "Pong");
-  SetWindowState(FLAG_VSYNC_HINT);
+  InitWindow(800, 600, "Pong"); //Sets up a window
+  SetWindowState(FLAG_VSYNC_HINT); //Sets up VSYNC to remove ghosting
 
-  while (!WindowShouldClose()) {
+  while (!WindowShouldClose()) { //While the window is open
     BeginDrawing();
-      ClearBackground(BLACK);
+      ClearBackground(BLACK); //Set background to black
       
-      DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 5, WHITE);
-      DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, WHITE);
-      DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, WHITE);
-      
+      DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 5, WHITE); //Draw ball
+      DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, WHITE); //Draw left paddle
+      DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, WHITE); //Draw right paddle
 
-      DrawFPS(10, 10);
+      DrawFPS(10, 10); //Show FPS
     EndDrawing();
   }
   
